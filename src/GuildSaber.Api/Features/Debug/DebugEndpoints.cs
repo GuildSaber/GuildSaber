8f00b67ebb21286f9a0e8aa5554649840520a308
Kuurama <74458092+Kuurama@users.noreply.github.com>
2026-01-05 02:45:18 +0100

    Rename refetch ranked score to recalculate player score

--- src/GuildSaber.Api/Features/Debug/DebugEndpoints.cs
+++ src/GuildSaber.Api/Features/Debug/DebugEndpoints.cs
@@ -7,7 +7,6 @@ using GuildSaber.Api.Features.Guilds.Members.Pipelines;
 using GuildSaber.Api.Features.Players.Pipelines;
 using GuildSaber.Api.Features.RankedMaps;
 using GuildSaber.Api.Features.RankedMaps.MapVersions;
-using GuildSaber.Api.Features.Scores.Pipelines;
 using GuildSaber.Api.Queuing;
 using GuildSaber.Api.Transformers;
 using GuildSaber.Common.Services.BeatSaver.Models.StrongTypes;
@@ -91,9 +90,9 @@ public class DebugEndpoints : IEndpoints
             .WithDescription("Recalculates member levels for all contexts the player is a member of.")
             .RequireManager();
 
-        group.MapPost("/refetch-player-ranked-scores/{playerId}", RefetchPlayerRankedScores)
-            .WithSummary("Refetch player ranked scores.")
-            .WithDescription("Refetches all ranked scores for the specified player.")
+        group.MapPost("/recalculate-player-scores/{playerId}", RecalculatePlayerScores)
+            .WithSummary("Recalculate player scores.")
+            .WithDescription("Recalculates all player scores for the specified player.")
             .RequireManager();
 
         group.MapPost("delete-member-point-stats/{playerId}", async (PlayerId playerId, ServerDbContext dbContext) =>
@@ -107,7 +106,7 @@ public class DebugEndpoints : IEndpoints
             .RequireManager();
     }
 
-    private static async Task<Ok> RefetchPlayerRankedScores(
+    private static async Task<Ok> RecalculatePlayerScores(
         PlayerId playerId, ServerDbContext dbContext,
         IBackgroundTaskQueue taskQueue,
         IServiceScopeFactory serviceScopeFactory)
