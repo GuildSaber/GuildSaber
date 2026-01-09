5365807e8e7e568d471d47e3c715bd95b38113a3
Kuurama <74458092+Kuurama@users.noreply.github.com>
2026-01-09 22:43:35 +0100

    Fix: BLScoreSyncWorker crashing because of dbcontext and compiled query model sharing

--- src/GuildSaber.Api/Features/Scores/BLScoreSyncWorker.cs
+++ src/GuildSaber.Api/Features/Scores/BLScoreSyncWorker.cs
@@ -8,7 +8,6 @@ using GuildSaber.Common.Services.BeatLeader.Models.StrongTypes;
 using GuildSaber.Database.Contexts.Server;
 using GuildSaber.Database.Models.Mappers.BeatLeader;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Caching.Hybrid;
 
 namespace GuildSaber.Api.Features.Scores;
@@ -49,13 +48,7 @@ public class BLScoreSyncWorker(
     protected override async Task ExecuteAsync(CancellationToken token)
     {
         await using var scope = serviceScopeFactory.CreateAsyncScope();
-
-        var baseOptions = scope.ServiceProvider.GetRequiredService<DbContextOptions<ServerDbContext>>();
-        await using var dbContext = new ServerDbContext(new DbContextOptionsBuilder<ServerDbContext>(baseOptions)
-            .ConfigureWarnings(w => w.Ignore(
-                RelationalEventId.CommandExecuted,
-                RelationalEventId.CommandExecuting))
-            .Options);
+        await using var dbContext = scope.ServiceProvider.GetRequiredService<ServerDbContext>();
 
         var cache = scope.ServiceProvider.GetRequiredService<HybridCache>();
         var pointStatsLogger = scope.ServiceProvider.GetRequiredService<ILogger<MemberPointStatsPipeline>>();
