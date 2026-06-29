377e3efda041bed02a377e5ee616a8d0b07d0e86
Kuurama <74458092+Kuurama@users.noreply.github.com>
2026-06-30 00:36:42 +0200

    hotfix(api): leaderboard sorting fix real real

--- src/GuildSaber.Api/Features/Leaderboards/Http/Endpoints.cs
+++ src/GuildSaber.Api/Features/Leaderboards/Http/Endpoints.cs
@@ -147,10 +147,11 @@ public static class LeaderboardExtensions
     {
         LeaderboardRequests.ERankedMapLeaderboardSorter.Points => query
             .OrderBy(order, x => x is ScoredRankedScore)
-            .ThenBy(order, x => x is ScoredRankedScore ? ((ScoredRankedScore)x).RawPoints : default)
             .ThenBy(order, x => x is PointGivingRankedScore ? ((PointGivingRankedScore)x).Rank : 0)
+            .ThenBy(order, x => x.EffectiveScore)
             .ThenBy(order, x => x.Id),
-        LeaderboardRequests.ERankedMapLeaderboardSorter.EffectiveScore => query.OrderBy(order, x => x.EffectiveScore)
+        LeaderboardRequests.ERankedMapLeaderboardSorter.EffectiveScore => query
+            .OrderBy(order, x => x.EffectiveScore)
             .ThenBy(order, x => x is PointGivingRankedScore ? ((PointGivingRankedScore)x).Rank : 0)
             .ThenBy(order, x => x.Id),
         _ => throw new ArgumentOutOfRangeException(nameof(sortBy), sortBy, null)
